4fff1c3c58c40f81179883fe9919febc1698e9b8
robinquca <155705374+robinquca@users.noreply.github.com>
2025-07-17 17:26:50 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -1,33 +1,23 @@
 #!/bin/bash
-#=================================================
-# File name: hook-feeds.sh
-# System Required: Linux
-# Version: 1.0
-# Lisence: MIT
-# Author: SuLingGG
-# Blog: https://mlapp.cn
-#=================================================
-# Svn checkout packages from immortalwrt's repository
+set -e
+set -x
 
-#5G信号插件，拨号工具，驱动
+# 清理旧目录并拉取 5G 插件支持
 rm -rf package/wwan
 git clone --depth=1 https://github.com/Siriling/5G-Modem-Support package/wwan
 
-# Add iStoreOS
-echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
-echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+# 添加 iStoreOS 源（如果还没添加）
+grep -q '^src-git istore' feeds.conf.default || echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+grep -q '^src-git nas ' feeds.conf.default || echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+grep -q '^src-git nas_luci ' feeds.conf.default || echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 
-pushd customfeeds
+# 设置 customfeeds 路径（如果存在才添加）
+if [[ -d customfeeds/packages ]]; then
+  sed -i '/src-git packages/d' feeds.conf.default
+  echo "src-link packages $(realpath customfeeds/packages)" >> feeds.conf.default
+fi
 
-# Set to local feeds
-pushd customfeeds/packages
-export packages_feed="$(pwd)"
-popd
-pushd customfeeds/luci
-export luci_feed="$(pwd)"
-popd
-sed -i '/src-git packages/d' feeds.conf.default
-echo "src-link packages $packages_feed" >> feeds.conf.default
-sed -i '/src-git luci/d' feeds.conf.default
-echo "src-link luci $luci_feed" >> feeds.conf.default
+if [[ -d customfeeds/luci ]]; then
+  sed -i '/src-git luci/d' feeds.conf.default
+  echo "src-link luci $(realpath customfeeds/luci)" >> feeds.conf.default
+fi
